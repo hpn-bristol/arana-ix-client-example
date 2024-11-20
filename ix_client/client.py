ceca94e9e03bdaa007527ae4c114121c9b31606d
Savvas Mantzouranidis <s.mantzouranidis@bristol.ac.uk>
2024-11-20 09:49:39 +0000

    Update client's documentation

--- ix_client/client.py
+++ ix_client/client.py
@@ -9,10 +9,10 @@ logging.basicConfig(format="%(levelname)s:     %(message)s", level=logging.INFO)
 
 
 class IxClient(object):
-    """An ORANOS Ix Client.
+    """Ix Client.
 
-    This class implements an ORANOS Ix client that enables xApp communication to their respective Ix server.
-    It uses socket.io websockets and long-polling requests to achieve minimal transmit latency.
+    This class implements an Ix Client that enables xApp communication to their respective Ix Interface.
+    It uses socket.io web sockets and long-polling requests to achieve minimal transmit latency.
 
     :param url: Root URL of the xApp's local Ix server.
     :param username: The `ix_username` provided by the Ix server after registering the xApp.
@@ -45,7 +45,7 @@ class IxClient(object):
 
     @property
     def is_connected(self) -> bool:
-        """Returns the current connection status of the Ix client."""
+        """Returns the current connection status of the Ix Client."""
         return self._is_connected
 
     def socketio_wrapper(func):
@@ -62,8 +62,8 @@ class IxClient(object):
     def connect(self, relation_id: str = ""):
         """Initiate the Ix client's connection to the Ix server.
 
-        :param relation_id: (Optional) If a relation_id is provided, the Ix server will create a 
-        pipeline to the remote Ix server and xApp found in the relation details.
+        :param relation_id: (Optional) If a relation_id is provided, the Ix Interface will create a 
+        pipeline to the remote Ix Interface and transmit the data as per the relation's details.
         """
         auth = {"ix_username": self._username, "ix_password": self._password}
         self._emit_path = "xapp_local_emit"
@@ -74,14 +74,14 @@ class IxClient(object):
 
     @socketio_wrapper
     def disconnect(self):
-        """Terminate the Ix client's connection to the Ix server."""
+        """Terminate the Ix Client's connection to the Ix Interface."""
         if self._is_connected:
             self._client.disconnect()
 
     @socketio_wrapper
     def send(self, data: dict):
-        """Transmit data to the Ix server. If the connection was made with a relation_id, 
-        the data will also be forwarded to the remote Ix server and xApp found in the relation details.
+        """Transmit data to the Ix Interface. If the connection was made with a relation_id provided, 
+        the data will be forwarded to the remote Ix Interface and xApp, as per the relation details.
 
         :param data: Data in the form of a dictionary.
 
